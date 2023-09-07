9bf305f18c98150991c7ce41ee897fca19506a2a
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-09-07 20:49:50 -0300

    Entity User e REST funcionando

--- src/main/java/br/com/workshop/WorkshopmongoApplication.java
+++ src/main/java/br/com/workshop/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package br.com.workshop.app;
+package br.com.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class WorkshopmongoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/br/com/workshop/controllers/UserController.java
@@ -0,0 +1,26 @@
+package br.com.workshop.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.workshop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = new ArrayList<>();
+		User maria = new User("1001", "Maria Brown", "maria@gmail.com");
+		User alex = new User("1002", "Alex Green", "alex@gmail.com");
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+}
