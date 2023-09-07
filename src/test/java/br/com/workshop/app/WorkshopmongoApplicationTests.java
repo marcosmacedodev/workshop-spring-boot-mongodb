138d1c194bb166baedd14c1098c24cadf838fa95
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-09-07 19:25:34 -0300

    Iniciando...

---
+++ src/test/java/br/com/workshop/app/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.workshop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
